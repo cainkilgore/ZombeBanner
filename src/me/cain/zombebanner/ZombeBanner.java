7a33e32c3b59f3362d57916d1a565a0803ccacd9
Jake King <imjake9@gmail.com>
2011-07-24 17:14:26 -0700

    Huge reformatting.

--- src/me/cain/zombebanner/ZombeBanner.java
+++ src/me/cain/zombebanner/ZombeBanner.java
@@ -1,7 +1,5 @@
 package me.cain.zombebanner;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.logging.Logger;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
@@ -15,65 +13,66 @@ import org.bukkit.util.config.Configuration;
 
 public class ZombeBanner extends JavaPlugin
 {
-	private final ZombeListener TheListener = new ZombeListener(this);
-	public Configuration config;
-	static final Logger log = Logger.getLogger("Minecraft");
-	public final String pluginname = "ZombeBanner";
-	public static PermissionHandler permissionHandler;
-        public boolean showMessages = true;
-        public boolean stopFly = true;
-        public boolean stopCheat = true;
+    private final ZombeListener TheListener = new ZombeListener(this);
+    public Configuration config;
+    static final Logger log = Logger.getLogger("Minecraft");
+    public final String pluginname = "ZombeBanner";
+    public static PermissionHandler permissionHandler;
+    public boolean showMessages = true;
+    public boolean stopFly = true;
+    public boolean stopCheat = true;
 
+    @Override
+    public void onDisable()
+    {
+        log.info("[" + pluginname + "] " + pluginname + " has been disabled.");
+    }
 
-        @Override
-	public void onDisable() 
-	{
-		log.info("[" + pluginname + "] " + pluginname + " has been disabled.");
-	}
+    @Override
+    public void onEnable()
+    {
+        config = this.getConfiguration();
+        PluginManager pm = getServer().getPluginManager();
+        log.info("[" + pluginname + "] " + pluginname + " has been enabled.");
+        log.info("[" + pluginname + "] Created by CainFoool");
+        pm.registerEvent(Event.Type.PLAYER_INTERACT, new ZombeListener(this), Priority.Normal, this);
+        pm.registerEvent(Event.Type.PLAYER_JOIN, TheListener, Priority.Normal, this);
+        setupPermissions();
+        ConfigurationCheck();
+    }
 
-        @Override
-	public void onEnable() 
-	{
-		config = this.getConfiguration();
-			PluginManager pm = getServer().getPluginManager();
-		log.info("[" + pluginname + "] " + pluginname + " has been enabled.");
-		log.info("[" + pluginname + "] Created by CainFoool");
-		pm.registerEvent(Event.Type.PLAYER_INTERACT, new ZombeListener(this), Priority.Normal, this);
-		pm.registerEvent(Event.Type.PLAYER_JOIN, TheListener, Priority.Normal, this);
-		setupPermissions();
-		ConfigurationCheck();
-	}
-	
-	  public static boolean PermissionCheck(String node, Player player) {
-		    if (permissionHandler != null) {
-		      return permissionHandler.has(player, node);
-		    }
-		    return player.hasPermission(node);
-		  }
-	
-	public void ConfigurationCheck() 
-	{
-                showMessages = config.getBoolean("config.showmessages", true);
-                stopFly = config.getBoolean("config.banfly", true);
-                stopCheat = config.getBoolean("config.bancheat", true);
-                config.save();
-	}
-	 
-	 private void setupPermissions() {
-		    if (permissionHandler != null) {
-		        return;
-		    }
-		    
-		    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
-		    
-		    if (permissionsPlugin == null) {
-		        log.info("Permission system not detected, defaulting to OP");
-		        return;
-		    }
-		    
-		    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-		    log.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
-		}
+    public static boolean PermissionCheck(String node, Player player)
+    {
+        if (permissionHandler != null) {
+            return permissionHandler.has(player, node);
+        }
+        return player.hasPermission(node);
+    }
+
+    public void ConfigurationCheck()
+    {
+        showMessages = config.getBoolean("config.showmessages", true);
+        stopFly = config.getBoolean("config.banfly", true);
+        stopCheat = config.getBoolean("config.bancheat", true);
+        config.save();
+    }
+
+    private void setupPermissions()
+    {
+        if (permissionHandler != null) {
+            return;
+        }
+
+        Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+        if (permissionsPlugin == null) {
+            log.info("Permission system not detected, defaulting to OP");
+            return;
+        }
+
+        permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+        log.info("Found and will use plugin " + ((Permissions) permissionsPlugin).getDescription().getFullName());
+    }
 }
 		
 // Created by CainFoool
--- src/me/cain/zombebanner/ZombeListener.java
+++ src/me/cain/zombebanner/ZombeListener.java
@@ -7,34 +7,36 @@ import org.bukkit.event.player.PlayerListener;
 
 public class ZombeListener extends PlayerListener
 {
-	ZombeBanner plugin;
-	Server server;
-	
-	public ZombeListener(ZombeBanner instance)
-	{
-		this.plugin = instance;
-	}
+    ZombeBanner plugin;
+    Server server;
 
-        @Override
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-		if(!ZombeBanner.PermissionCheck("zombe.allowmods", player)) {
-                        if(plugin.stopFly)
-                            player.sendMessage("ßf ßf ß1 ß0 ß2 ß4");
-                        if(plugin.stopCheat)
-                            player.sendMessage("ßf ßf ß2 ß0 ß4 ß8");
-			if(plugin.showMessages) {
-                            player.sendMessage("Zombe has been disabled.");
-			} else {
-				return;
-			}
-		}
-		else {
-			if(plugin.showMessages) {
-                            player.sendMessage("You are allowed to use Zombe!");
-			} else {
-				return;
-                        }
-		}
-	}
+    public ZombeListener(ZombeBanner instance)
+    {
+        this.plugin = instance;
+    }
+
+    @Override
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        Player player = event.getPlayer();
+        if (!ZombeBanner.PermissionCheck("zombe.allowmods", player)) {
+            if (plugin.stopFly) {
+                player.sendMessage("ßf ßf ß1 ß0 ß2 ß4");
+            }
+            if (plugin.stopCheat) {
+                player.sendMessage("ßf ßf ß2 ß0 ß4 ß8");
+            }
+            if (plugin.showMessages) {
+                player.sendMessage("Zombe has been disabled.");
+            } else {
+                return;
+            }
+        } else {
+            if (plugin.showMessages) {
+                player.sendMessage("You are allowed to use Zombe!");
+            } else {
+                return;
+            }
+        }
+    }
 }
