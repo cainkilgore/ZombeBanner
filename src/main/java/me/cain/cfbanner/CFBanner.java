b3c8c238e1ce0365234a336506dcfd29689b78da
Jesse Leskinen <jessenic@digiex.net>
2011-08-10 18:20:32 +0300

    Adding Zombe logging, ability to disable spout, changed config to use getBoolean and getString instead of getProperty, /cfbanner command reloads config, ability to log thrown errors, update check in thread now, ability to use without spout, and more that I cant remember

--- src/main/java/me/cain/cfbanner/CFBanner.java
+++ src/main/java/me/cain/cfbanner/CFBanner.java
@@ -22,126 +22,211 @@ import org.bukkit.util.config.Configuration;
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
-public class CFBanner extends JavaPlugin
-{
+public class CFBanner extends JavaPlugin {
 	private final CFListener TheListener = new CFListener(this);
-	private final SpoutL SListener = new SpoutL(this);
+	private SpoutL SListener = null;
 	Player player;
 	public static Configuration config;
 	Logger console = Logger.getLogger("Minecraft");
 	String pName = "CFBanner";
 	public static PermissionHandler permissionHandler;
 	Boolean spout = false;
-	String testmaven = "lolw";
-	
-	// Maven support! Yay!
+	public CFLogger logger = new CFLogger(this);
 
-
-	public void onDisable() 
-	{
+	public void onDisable() {
 		console.info("[" + pName + "] " + pName + " has been disabled.");
 		console.info("[" + pName + "] " + pName + " was created by CainFoool");
 	}
 
-	public void onEnable() 
-	{
+	public void onEnable() {
 		config = this.getConfiguration();
 		PluginManager pm = getServer().getPluginManager();
-		console.info("[" + pName + "] " + pName + " has been enabled.");
-		console.info("[" + pName + "] Created by CainFoool");
-		if(config.getBoolean("config.checkforupdates", true)) { VersionCheck(); }
+		if (pm.getPlugin("Spout") != null && config.getBoolean("config.usespout",true)) {
+			spout = true;
+			SListener = new SpoutL(this);
+		} else {
+			console.info("[" + pName
+					+ "] Spout is disabled!");
+			spout = false;
+			SListener = null;
+		}
+		if (config.getBoolean("config.checkforupdates", true)) {
+			VersionCheck();
+		}
 		setupPermissions();
-		pm.registerEvent(Event.Type.PLAYER_JOIN, TheListener, Priority.Normal, this);
-		pm.registerEvent(Type.CUSTOM_EVENT, SListener, Priority.Low, this);
+		pm.registerEvent(Event.Type.PLAYER_JOIN, TheListener, Priority.Normal,
+				this);
+		if (spout && SListener != null) {
+			pm.registerEvent(Type.CUSTOM_EVENT, SListener, Priority.Low, this);
+		}
 		config.load();
 		ConfigurationCheck();
+		console.info("[" + pName + "] " + pName + " has been enabled.");
+		console.info("[" + pName + "] Created by CainFoool");
 	}
-	
-	  public static boolean PermissionCheck(String node, Player player) {
-		    if (permissionHandler != null) {
-		      return permissionHandler.has(player, node);
-		    }
-		    return player.hasPermission(node);
-		  }
-	
-	public void ConfigurationCheck() 
-	{
-		if(config.getProperty("config.showmessages") == null)
-		{
-		     config.setProperty("config.showmessages", "true");
-		     config.save();
+
+	public static boolean PermissionCheck(String node, Player player) {
+		if (permissionHandler != null) {
+			return permissionHandler.has(player, node);
 		}
-		if(config.getProperty("config.checkforupdates") == null)
-		{
-			config.setProperty("config.checkforupdates", "true");
+		return player.hasPermission(node);
+	}
+
+	public void ConfigurationCheck() {
+		if (config.getProperty("config.showmessages") == null) {
+			config.setProperty("config.showmessages", true);
 			config.save();
 		}
-		if(config.getProperty("config.cfdisabledmessage") == null)
-		{
-			config.setProperty("config.cfdisabledmessage", "Zombe is disabled for you.");
+		if (config.getProperty("config.checkforupdates") == null) {
+			config.setProperty("config.checkforupdates", true);
 			config.save();
 		}
-		
-		  String file = this.getDataFolder().toString()+"/config.yml";
-		  File yml = new File(file);
-		  if (!yml.exists()) {
-			  new File(this.getDataFolder().toString()).mkdir();
-			  try {
-				  yml.createNewFile();
-			  } catch (IOException e) {
-				  e.printStackTrace();
-			  	}
-		  	}
+		if (config.getProperty("config.logerrors") == null) {
+			config.setProperty("config.logerrors", true);
+			config.save();
 		}
-	 
-	 private void setupPermissions() {
-		    if (permissionHandler != null) {
-		        return;
-		    }
-		    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
-		    if (permissionsPlugin == null) {
-		        console.info("[" + pName + "] Permissions not detected. Defaulting to OP.");
-		        return;
-		    }
-		    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
-		    console.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+		if (config.getProperty("config.logzombe") == null) {
+			config.setProperty("config.logzombe", true);
+			config.save();
 		}
-	 
-	 private void VersionCheck() {
-	        try {
-	            URL url = new URL("http://dl.dropbox.com/u/7186172/cfbanner_version.txt");
-	            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-	            String str;
-	            while ((str = in.readLine()) != null) {
-	                int version = Integer.parseInt(str);
-	                if (version > 170){
-	                    console.info("[CFBanner] A new update is available!");
-	                    console.info("[CFbanner] Download at: http://bit.ly/rj5iJl");
-	                    break;
-	                }
-	            }
-	            in.close();
-	        }
-	        catch (Exception e) { e.printStackTrace(); }
-	 }
-	 
-	 public boolean onCommand(CommandSender sender, Command cmd, String label, String [] args) {
-		 if(label.equals("cfbanner")) {
-			 if(!(sender instanceof Player)) {
-				 sender.sendMessage(ChatColor.DARK_RED + "You can only check settings in-game.");
-			 } else {
-		 if(!CFBanner.PermissionCheck("cf.command", (Player) sender)) {
-			 sender.sendMessage("You do not have permission to use this command!");
-		 } else {
-			 sender.sendMessage(ChatColor.GREEN + "=====Server CFBanner Settings=====");
-			 sender.sendMessage(ChatColor.GREEN + "| Show Messages: " + ChatColor.WHITE + CFBanner.config.getProperty("config.showmessages").toString());
-			 sender.sendMessage(ChatColor.GREEN + "| Version Checking: " + ChatColor.WHITE + CFBanner.config.getProperty("config.checkforupdates").toString());
-			 sender.sendMessage(ChatColor.GREEN + "| CFBanner Disabled Message: " + ChatColor.WHITE + CFBanner.config.getProperty("config.zombedisabledmessage").toString());
-			 sender.sendMessage(ChatColor.GREEN + "===============================");
-		 }
+		if (config.getProperty("config.cfdisabledmessage") == null) {
+			config.setProperty("config.cfdisabledmessage",
+					"Zombe is disabled for you.");
+			config.save();
+		}
+		if (config.getProperty("config.usespout") == null) {
+			config.setProperty("config.usespout", true);
+			config.save();
+		}
+
+		String file = this.getDataFolder().toString() + "/config.yml";
+		File yml = new File(file);
+		if (!yml.exists()) {
+			new File(this.getDataFolder().toString()).mkdir();
+			try {
+				yml.createNewFile();
+			} catch (IOException e) {
+				logger.write(e);
+			}
+		}
 	}
-		 }
+
+	private void setupPermissions() {
+		if (permissionHandler != null) {
+			return;
+		}
+		Plugin permissionsPlugin = this.getServer().getPluginManager()
+				.getPlugin("Permissions");
+		if (permissionsPlugin == null) {
+			console.info("[" + pName
+					+ "] Permissions not detected. Defaulting to OP.");
+			return;
+		}
+		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+		console.info("Found and will use plugin "
+				+ ((Permissions) permissionsPlugin).getDescription()
+						.getFullName());
+	}
+
+	private void VersionCheck() {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					URL url = new URL(
+							"http://dl.dropbox.com/u/7186172/cfbanner_version.txt");
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(url.openStream()));
+					String str;
+					while ((str = in.readLine()) != null) {
+						int version = Integer.parseInt(str);
+						if (version > 170) {
+							console.info("[CFBanner] A new update is available!");
+							console.info("[CFbanner] Download at: http://bit.ly/rj5iJl");
+							break;
+						}
+					}
+					in.close();
+				} catch (Exception e) {
+					logger.write(e);
+				}
+			}
+		}.start();
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (label.equalsIgnoreCase("cfbanner")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.DARK_RED
+						+ "You can only check settings in-game.");
+			} else {
+				if (!CFBanner.PermissionCheck("cf.command", (Player) sender)) {
+					sender.sendMessage("You do not have permission to use this command!");
+				} else {
+					config.load(); //reloads config
+					sender.sendMessage(ChatColor.GREEN
+							+ "=====Server CFBanner Settings=====");
+					sender.sendMessage(ChatColor.GREEN
+							+ "| Show Messages: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getBoolean("config.showmessages",
+									true));
+					sender.sendMessage(ChatColor.GREEN
+							+ "| Version Checking: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getBoolean(
+									"config.checkforupdates", true));
+					sender.sendMessage(ChatColor.GREEN
+							+ "| Error Logging: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getBoolean("config.logerrors",
+									true));
+					sender.sendMessage(ChatColor.GREEN
+							+ "| Zombe Logging: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getBoolean("config.logzombe",
+									true));
+					sender.sendMessage(ChatColor.GREEN
+							+ "| Use Spout: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getBoolean("config.usespout",
+									true));
+					sender.sendMessage(ChatColor.GREEN
+							+ "| CFBanner Disabled Message: "
+							+ ChatColor.WHITE
+							+ CFBanner.config.getString(
+									"config.zombedisabledmessage",
+									"Zombe is disabled for you."));
+					sender.sendMessage(ChatColor.GREEN
+							+ "===============================");
+				}
+			}
+		} else if (label.equalsIgnoreCase("zombe-icon")) {
+			if (sender instanceof Player) {
+				if (!CFBanner.PermissionCheck("cf.notify.exempt",
+						(Player) sender)) {
+					for (Player p : this.getServer().getOnlinePlayers()) {
+						if (CFBanner.PermissionCheck("cf.notify",
+								(Player) sender)) {
+							p.sendMessage(ChatColor.RED
+									+ ((Player) sender).getDisplayName()
+									+ " uses zombe mods!"
+									+ ChatColor.GRAY
+									+ " This may be false positive if you did not get this message when the player logged in.");
+						}
+					}
+					if (CFBanner.config.getBoolean("config.logzombe", true)) {
+						logger.write(((Player) sender).getDisplayName() + " ("
+								+ ((Player) sender).getName()
+								+ ") used zombe mods.");
+					}
+				}
+			}
+			return true;
+		}
 		return false;
-}
-	 
+	}
+
 }
--- src/main/java/me/cain/cfbanner/SpoutL.java
+++ src/main/java/me/cain/cfbanner/SpoutL.java
@@ -8,46 +8,56 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SpoutL extends SpoutListener {
 	CFBanner plugin;
-	private boolean Color;
-	
-    public SpoutL(CFBanner plugin) {
-        this.plugin = plugin;
-    }
-	
+
+	public SpoutL(CFBanner plugin) {
+		this.plugin = plugin;
+	}
+
 	public void onSpoutCraftEnable(SpoutCraftEnableEvent event) {
-        if(!event.getPlayer().isSpoutCraftEnabled()) {
-        	return;
-        } else {
-        	if(CFBanner.PermissionCheck("cf.allowfly", (SpoutPlayer) event.getPlayer()) || CFBanner.PermissionCheck("cf.allowcheat", (SpoutPlayer) event.getPlayer()))
-        	{
-        		if(CFBanner.config.getProperty("config.showmessages").equals("true")) { 
-			final SpoutPlayer splayer = (SpoutPlayer) event.getPlayer();
-			addText("FFFFFF", "CFBanner is on!", "bottom-right", splayer);
-				splayer.sendNotification("CFBanner", "You're allowed mods!", Material.DIAMOND);
-				} else { return; }
-        	}
-        	}
-        }
-    
-    public void addText(String color, String text, String position, SpoutPlayer sp) {
-        GenericLabel spouttext = new GenericLabel("");
-        if(position.toString().equals("health")) {  
-        	spouttext.setHexColor(Integer.parseInt(color, 16)).setX(209).setY(211);
-        }
-        if(position.toString().equals("top-right")) {
-        	spouttext.setHexColor(Integer.parseInt(color, 16)).setX(300).setY(5);
-        }
-        if(position.toString().equals("top-left")) {
-        	spouttext.setHexColor(Integer.parseInt(color, 16)).setX(3).setY(5);
-        }
-        if(position.toString().equals("bottom-left")) {
-        	spouttext.setHexColor(Integer.parseInt(color, 16)).setX(3).setY(220);
-        }
-        if(position.toString().equals("bottom-right")) {
-        	spouttext.setHexColor(Integer.parseInt(color, 16)).setX(315).setY(230);
-        }
-        spouttext.setText(text);
-        spouttext.setDirty(true);
-        sp.getMainScreen().attachWidget(spouttext);
-    }
+		if (!event.getPlayer().isSpoutCraftEnabled()) {
+			return;
+		} else {
+			if (CFBanner.PermissionCheck("cf.allowfly",
+					(SpoutPlayer) event.getPlayer())
+					|| CFBanner.PermissionCheck("cf.allowcheat",
+							(SpoutPlayer) event.getPlayer())) {
+				if (CFBanner.config.getBoolean("config.showmessages", true)) {
+					final SpoutPlayer splayer = (SpoutPlayer) event.getPlayer();
+					addText("FFFFFF", "CFBanner is on!", "bottom-right",
+							splayer);
+					splayer.sendNotification("CFBanner",
+							"You're allowed mods!", Material.DIAMOND);
+				} else {
+					return;
+				}
+			}
+		}
+	}
+
+	public void addText(String color, String text, String position,
+			SpoutPlayer sp) {
+		GenericLabel spouttext = new GenericLabel("");
+		if (position.toString().equals("health")) {
+			spouttext.setHexColor(Integer.parseInt(color, 16)).setX(209)
+					.setY(211);
+		}
+		if (position.toString().equals("top-right")) {
+			spouttext.setHexColor(Integer.parseInt(color, 16)).setX(300)
+					.setY(5);
+		}
+		if (position.toString().equals("top-left")) {
+			spouttext.setHexColor(Integer.parseInt(color, 16)).setX(3).setY(5);
+		}
+		if (position.toString().equals("bottom-left")) {
+			spouttext.setHexColor(Integer.parseInt(color, 16)).setX(3)
+					.setY(220);
+		}
+		if (position.toString().equals("bottom-right")) {
+			spouttext.setHexColor(Integer.parseInt(color, 16)).setX(315)
+					.setY(230);
+		}
+		spouttext.setText(text);
+		spouttext.setDirty(true);
+		sp.getMainScreen().attachWidget(spouttext);
+	}
 }
